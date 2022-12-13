ab85b6ffe8af95f4b4c2a0eb11bcd40ef09632c2
Jose A. Perez <69980014+jabjack@users.noreply.github.com>
2022-12-12 21:14:54 -0600

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+
+  curso = 'Curso Spring y Angular';
+
+  profesor: string = " Jose Perez ";
+}
